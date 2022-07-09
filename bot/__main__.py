b8d7c9db706675ae0c19f0d8a9eae15df7b3bbcb
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-07-09 14:42:37 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -66,10 +66,10 @@ def stats(update, context):
 def start(update, context):
     buttons = ButtonMaker()
     
-    buttons.buildbutton("Report Group", "https://t.me/Mltb_chat_unofficial")
-    buttons.buildbutton("Repo", "https://github.com/Dawn-India/Z-Mirror")
-    buttons.buildbutton("Mirror Group", "https://t.me/z_mirror")
-    buttons.buildbutton("Owner", "https://t.me/dawn_in")
+    buttons.buildbutton("1", "https://Google.com/")
+    buttons.buildbutton("2", "https://Google.com/")
+    buttons.buildbutton("3", "https://Google.com/")
+    buttons.buildbutton("4", "https://Google.com/")
     
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
